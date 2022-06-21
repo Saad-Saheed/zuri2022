9b807094900c8fda74f25762740cc3b228360c76
Saad-Saheed <janbaoda@gmail.com>
2022-06-21 14:24:54 +0100

    calculator validation

--- calculator/index.js
+++ calculator/index.js
@@ -1,4 +1,14 @@
-const first_number = window.prompt("Enter First Number");
-const second_number = window.prompt("Enter Second Number");
+let operators = ['+', '-', '*', '/'];
+const first_number = Number.parseFloat(window.prompt("Enter First Number"));
+const second_number = Number.parseFloat(window.prompt("Enter Second Number"));
 const sel_operators = window.prompt("Enter one the following operators +, -, /, *");
-window.alert(`Result: ${eval(first_number+""+sel_operators+""+second_number )}`);
+
+if(Number.isNaN(first_number)){
+    alert('invalid first Number');
+}else if(Number.isNaN(second_number)){
+    alert('invalid second Number');
+}else if(!operators.includes(sel_operators)){
+    alert('invalid operator supplied');
+}else{
+    window.alert(`Result: ${eval(first_number+""+sel_operators+""+second_number )}`);
+}
